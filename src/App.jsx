f097b88162b581c992abd292c96b6ebb3c01a6e0
Mosayeb09 <mosayebhossain09@gmail.com>
2024-02-11 17:42:02 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import Blogs from './components/Blogs/Blogs'
+import Header from './components/Header/Head'
+
+
+function App() {
+  
+
+  return (
+    <>
+   
+      <Header></Header>
+      <Blogs></Blogs>
+     
+      
+    </>
+  )
+}
+
+export default App
